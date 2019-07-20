3f5a264bc86940967e2f103e1008245d671825a5
DJ Zaragoza <zaragoza0126@gmail.com>
2019-07-20 14:53:29 -0700

    modification on a few files

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -44,7 +44,7 @@ class App extends Component {
   render () {
     return (
       <div className='App'>
-        <h1>Smurfs 2.0 W/ Redux</h1>
+        <h1>Smurfs Village 2.0 - Redux</h1>
         {this.props.smurfs.map(smurf => {
           return (
             <div>
