087504d87fe8d49ec417d930d2bc5ee626de7019
DJ Zaragoza <zaragoza0126@gmail.com>
2019-07-20 11:52:46 -0700

    still debugging

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,9 +1,82 @@
-import { FETCH, FETCH_SUCCESS, FETCH_FAILURE, ADD, DELETE } from '../actions'
+import {
+  LOAD_START,
+  ADD_SMURF,
+  UPDATE_SMURF,
+  DELETE_SMURF,
+  SMURF_FAILURE,
+  SMURF_SUCCESS
+} from '../actions'
+/*
+  Be sure to import in all of the action types from `../actions`
+*/
 
 const initialState = {
   smurfs: [],
   fetchingSmurfs: false,
-  error: ''
+  addSmurf: false,
+  updateSmurf: false,
+  deleteSmurf: false,
+  error: null
+}
+/*
+ Your initial/default state for this project could *Although does not have to* look a lot like this
+ {
+   smurfs: [],
+   fetchingSmurfs: false
+   addingSmurf: false
+   updatingSmurf: false
+   deletingSmurf: false
+   error: null
+
+ }
+*/
+
+export const reducer = (state = initialState, action) => {
+  console.log('reducer', action)
+  switch (action.type) {
+    case LOAD_START:
+      return {
+        ...state,
+        error: null,
+        fetchingSmurfs: true
+      }
+    case ADD_SMURF:
+      return {
+        ...state,
+        error: null,
+        fetchingSmurfs: false,
+        smurfs: [...state.smurfs]
+      }
+    case UPDATE_SMURF:
+      return {
+        ...state,
+        error: null,
+        fetchingSmurfs: false,
+        smurfs: [...state.smurfs]
+      }
+    case DELETE_SMURF:
+      return {
+        ...state,
+        error: null,
+        fetchingSmurfs: false,
+        smurfs: [...state.smurfs]
+      }
+    case SMURF_FAILURE:
+      return {
+        ...state,
+        error: action.payload,
+        fetchingSmurfs: false
+      }
+    case SMURF_SUCCESS:
+      return {
+        ...state,
+        error: null,
+        fetchingSmurfs: false,
+        smurfs: action.payload
+      }
+    default:
+      return state
+  }
 }
 
 /*
@@ -13,43 +86,3 @@ const initialState = {
   There is no need for 'combineReducers' in this project.
   Components can then read your store as, `state` and not `state.fooReducer`.
 */
-
-const reducer = (state = initialState, action) => {
-  switch (action.type) {
-    case FETCH:
-      return {
-        ...state,
-        fetchingSmurfs: true,
-        error: ''
-      }
-    case FETCH_SUCCESS:
-      return {
-        ...state,
-        smurfs: action.payload,
-        fetchingSmurfs: false,
-        error: ''
-      }
-    case FETCH_FAILURE:
-      return {
-        ...state,
-        error: action.payload,
-        isLoading: false
-      }
-    case ADD:
-      return {
-        ...state,
-        smurfs: action.payload,
-        error: ''
-      }
-    case DELETE:
-      return {
-        ...state,
-        smurfs: action.payload,
-        error: ''
-      }
-    default:
-      return state
-  }
-}
-
-export default reducer
